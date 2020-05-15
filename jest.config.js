f5761327c7edffacbacedd1efbe8097a1b1b633b
WoeOm <1015996366@qq.com>
2020-05-16 00:01:00 +0800

    config compiler Options

--- jest.config.js
+++ jest.config.js
@@ -6,28 +6,15 @@ const config = require('@polkadot/dev/config/jest');
 
 module.exports = Object.assign({}, config, {
   moduleNameMapper: {
+    '@darwinia/api(.*)$': '<rootDir>/packages/api/src/$1',
+    '@darwinia/api-derive(.*)$': '<rootDir>/packages/api-derive/src/$1',
     '@darwinia/types(.*)$': '<rootDir>/packages/interfaces/src/$1',
-    '@polkadot/api-contract(.*)$': '<rootDir>/packages/api-contract/src/$1',
-    '@polkadot/api-derive(.*)$': '<rootDir>/packages/api-derive/src/$1',
-    // eslint-disable-next-line sort-keys
-    '@polkadot/api(.*)$': '<rootDir>/packages/api/src/$1',
-    '@polkadot/metadata(.*)$': '<rootDir>/packages/metadata/src/$1',
-    '@polkadot/rpc-(core|provider)(.*)$': '<rootDir>/packages/rpc-$1/src/$2',
-    '@polkadot/types-known(.*)$': '<rootDir>/packages/types-known/src/$1',
-    // eslint-disable-next-line sort-keys
-    '@polkadot/types(.*)$': '<rootDir>/packages/types/src/$1'
+    '@polkadot/api-options(.*)$': '<rootDir>/packages/api-options/src/$1'
   },
   modulePathIgnorePatterns: [
-    '<rootDir>/packages/api/build',
     '<rootDir>/packages/api-derive/build',
-    '<rootDir>/packages/api-contract/build',
-    '<rootDir>/packages/interfaces/build',
-    '<rootDir>/packages/metadata/build',
-    '<rootDir>/packages/rpc-core/build',
-    '<rootDir>/packages/rpc-provider/build',
-    '<rootDir>/packages/types/build',
-    '<rootDir>/packages/types-known/build'
+    '<rootDir>/packages/interfaces/build'
   ],
   resolver: './jest.resolver.js',
-  testTimeout: 30000
+  testTimeout: 100000
 });
