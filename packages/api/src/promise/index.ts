7b0e08faab58a452df1df5349921886e4f1ed510
WoeOm <1015996366@qq.com>
2020-05-15 23:51:26 +0800

    extends api

--- packages/api/src/promise/index.ts
+++ packages/api/src/promise/index.ts
@@ -2,7 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import ApiPromise, { decorateMethod } from './Api';
+import ApiPromise, { decorateMethod } from '@polkadot/api/promise';
+import { ApiOptions } from '@polkadot/api/types';
+import initOptions from '@darwinia/api-options';
 
-export default ApiPromise;
+export default class DarwiniaApiPromise extends ApiPromise {
+  constructor (options?: ApiOptions) {
+    super({ ...initOptions, ...options });
+  }
+}
+
 export { decorateMethod };
--- packages/api/src/rx/index.ts
+++ packages/api/src/rx/index.ts
@@ -2,7 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import ApiRx, { decorateMethod } from './Api';
+import ApiRx, { decorateMethod } from '@polkadot/api/rx';
+import { ApiOptions } from '@polkadot/api/types';
+import initOptions from '@darwinia/api-options';
 
-export default ApiRx;
+export default class DarwiniaApiRx extends ApiRx {
+  constructor (options?: ApiOptions) {
+    super({ ...initOptions, ...options });
+  }
+}
+
 export { decorateMethod };
