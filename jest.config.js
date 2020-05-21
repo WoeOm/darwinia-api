20ea0fba4c94cb0c783f6aab1caf73236d9a4906
WoeOm <1015996366@qq.com>
2020-05-21 10:20:26 +0800

    change package name

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = Object.assign({}, config, {
     '@darwinia/api-derive(.*)$': '<rootDir>/packages/api-derive/src/$1',
     '@darwinia/api-options(.*)$': '<rootDir>/packages/api-options/src/$1',
     // eslint-disable-next-line sort-keys
-    '@darwinia/api(.*)$': '<rootDir>/packages/api/src/$1',
+    '@darwinia/apps-api(.*)$': '<rootDir>/packages/api/src/$1',
     '@darwinia/types(.*)$': '<rootDir>/packages/interfaces/src/$1'
   },
   modulePathIgnorePatterns: [
